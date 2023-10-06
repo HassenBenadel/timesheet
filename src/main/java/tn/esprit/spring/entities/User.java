07071947de5ef82232df6211196a88d7c1506ed7
Hassen <mohamedhassen.benadel@esprit.tn>
2023-10-06 11:12:27 +0100

    ajout cmmntr

--- src/main/java/tn/esprit/spring/entities/User.java
+++ src/main/java/tn/esprit/spring/entities/User.java
@@ -24,7 +24,7 @@ public class User implements Serializable {
 	@Id
 	@GeneratedValue (strategy = GenerationType.IDENTITY)
 	private Long id;   
-	
+	//cmntr
 	private String firstName; 
 	
 	private String lastName;
